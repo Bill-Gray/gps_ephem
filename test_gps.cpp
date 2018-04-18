934d25d8505d6ebf26037d6be04b15071084dd6b
Bill-Gray <pluto@projectpluto.com>
2018-04-18 19:57:26 -0400

    Now can show GPS positions from TLEs if a TLE file is specified on the command line

--- test_gps.cpp
+++ test_gps.cpp
@@ -8,12 +8,21 @@ int main( const int argc, const char **argv)
       {
       double locs[MAX_N_GPS_SATS * 3];
       double *tptr = locs;
-      int i;
+      const double mjd = atof( argv[1]);
+      int i, rval;
 
-      printf( "rval = %d\n", get_gps_positions( locs, NULL, atof( argv[1])));
+      for( i = 0; i < MAX_N_GPS_SATS * 3; i++)
+         locs[i] = 0.;
+      if( argc > 2)
+         rval = get_gps_positions_from_tle( argv[2], locs, mjd);
+      else
+         rval = get_gps_positions( locs, NULL, mjd);
+      printf( "rval = %d\n", rval);
       for( i = 0; i < MAX_N_GPS_SATS; i++, tptr += 3)
          if( tptr[0] || tptr[1] || tptr[2])
-            printf( "%2d: %14.6f%14.6f%14.6f\n", i, tptr[0], tptr[1], tptr[2]);
+            printf( "%-4s %14.6f%14.6f%14.6f\n",
+                        desig_from_index( i),
+                        tptr[0], tptr[1], tptr[2]);
       free_cached_gps_positions( );
       }
    return( 0);
