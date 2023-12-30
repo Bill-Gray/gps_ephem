b97b89c1f137337f00e8c98d4b80ed4ae8ddb34d
Bill-Gray <pluto@projectpluto.com>
2023-12-30 11:21:24 -0500

    The data file I14.ATX has been moved,  updated to I20.ATX,  and now includes data on BeiDou (Chinese),  Indian,  and Japanese (QZSS) satellites.

--- names.cpp
+++ names.cpp
@@ -31,7 +31,7 @@ satellite today and a replacement satellite next year.  So we
 need some sort of cross-designation table to give you the 'real'
 designations.
 
-   The file ftp://ftp.aiub.unibe.ch/BSWUSER52/GEN/I14.ATX
+   The file ftp://ftp.aiub.unibe.ch/BSWUSER54/REF/I20.ATX
 you those cross-designations,  along with a lot of GNSS antenna
 data not relevant to this purpose.
 
@@ -42,7 +42,7 @@ BeiDou 'C', and the new Japanese satellites 'J'.  Presumably, future
 constellations will use other letters.  This all ought to work, as
 long as there are 99 or fewer satellites to a constellation.
 
-Anyway.  This code looks for lines such as the following in I14.ATX,
+Anyway.  This code looks for lines such as the following in I20.ATX,
 
 BLOCK IIA           G01                 G032      1992-079A TYPE / SERIAL NO
   1992    11    22     0     0    0.0000000                 VALID FROM
@@ -81,7 +81,7 @@ static void output_mjd( const char *date)
 
 #define MAX_REMAPS 100000
 
-/* I14.ATX has international (YYYY-NNNA) designations,  but not the NORAD
+/* I20.ATX has international (YYYY-NNNA) designations,  but not the NORAD
 five-digit designations.  Those are gathered from Jonathan McDowell's
 "master satellite list",  updated "every few months",  at
 
@@ -119,6 +119,12 @@ static int get_norad_number( const char *intl_id)
          rval = atoi( tptr);
       tptr += 21;
       }
+   if( !rval)
+      {
+      fprintf( stderr, "Couldn't find '%s'\n", intl_id);
+      fprintf( stderr, "Update 'satcat.html' and try again\n");
+      exit( -1);
+      }
    return( rval);
 }
 
@@ -136,72 +142,17 @@ static void output_line( const char *name, const char *from, const char *until)
                len, name);
 }
 
-/* https://en.wikipedia.org/wiki/List_of_BeiDou_satellites can aid in adding
-to this list as new BeiDou satellites are added.  You still have to get the
-international ID elsewhere.         */
-
-static const char *beidou_and_qzs =
-   "00000 99999 C01      2010-001A   36287 Beidou 3\n"       /* Compass-G1; geo 140 E */
-   "00000 99999 C02      2012-059A   38953 Beidou 16\n"      /* Compass-G6: geo 80 E  */
-   "00000 99999 C03      2010-024A   36590 Beidou 4\n"       /* Compass-G7: geo 110.5 E */
-   "00000 99999 C04      2010-057A   37210 Beidou 6\n"       /* Compass-G4: geo 160 E */
-   "00000 99999 C05      2012-008A   38091 Beidou 11\n"      /* Compass-G5: geo 58.75 E */
-   "00000 99999 C06      2010-036A   36828 Beidou 5\n"       /* Compass-IGSO1 : 55 incl, 118 E */
-   "00000 99999 C07      2010-068A   37256 Beidou 7\n"       /* Compass-IGSO2 : 55 incl, 118 E */
-   "00000 99999 C08      2011-013A   37384 Beidou 8\n"       /* Compass-IGSO3 : 55 incl, 118 E */
-   "00000 99999 C09      2011-038A   37763 Beidou 9\n"       /* Compass-IGSO4 : 55 incl, 95 E */
-   "00000 99999 C10      2011-073A   37948 Beidou 10\n"      /* Compass-IGSO5 : 55 incl, 95 E */
-   "00000 99999 C11      2012-018A   38250 Beidou 12\n"      /* Compass-M3: slot A07         */
-   "00000 99999 C12      2012-018B   38251 Beidou 13\n"      /* Compass-M4: slot A08         */
-   "00000 99999 C13      2016-021A   41434 Beidou IGSO-6\n"  /* Compass-IGSO6 : 55 incl, 95 E */
-   "00000 99999 C14      2012-050B   38775 Beidou 15\n"      /* Compass-M6: slot B04         */
-   "00000 99999 C16      2018-057A   43539 Beidou IGSO-7\n"  /* Compass-M6: slot B04         */
-   "00000 99999 C19      2017-069A   43001 Beidou-3 M1\n"
-   "00000 99999 C20      2017-069B   43002 Beidou-3 M2\n"
-   "00000 99999 C21      2018-018B   43208 Beidou-3 M6\n"
-   "00000 99999 C22      2018-018A   43207 Beidou-3 M5\n"
-   "00000 99999 C23      2018-062A   43581 Beidou-3 M9\n"
-   "00000 99999 C24      2018-062B   43582 Beidou-3 M10\n"
-   "00000 99999 C25      2018-067B   43603 Beidou-3 M12\n"
-   "00000 99999 C26      2018-067A   43602 Beidou-3 M11\n"
-   "00000 99999 C27      2018-003A   43107 Beidou-3 M3\n"
-   "00000 99999 C28      2018-003B   43108 Beidou-3 M4\n"
-   "00000 99999 C29      2018-029A   43245 Beidou-3 M7\n"
-   "00000 99999 C30      2018-029B   43246 Beidou-3 M8\n"
-   "00000 99999 C32      2018-072A   43622 Beidou-3 M13\n"
-   "00000 99999 C33      2018-027B   43623 Beidou-3 M14\n"
-   "00000 99999 C34      2018-078B   43648 Beidou-3 M16\n"
-   "00000 99999 C35      2018-078A   43647 Beidou-3 M15\n"
-   "00000 99999 C36      2018-093A   43706 Beidou-3 M17\n"
-   "00000 99999 C37      2018-093B   43707 Beidou-3 M18\n"
-   "00000 99999 C38      2019-023A   44204 Beidou-3 IGSO-1\n"
-   "00000 99999 C39      2019-035A   44337 Beidou-3 IGSO-2\n"
-   "00000 99999 C40      2019-073A   44709 Beidou-3 IGSO-3\n"
-   "00000 99999 C41      2019-090A   44864 Beidou-3 M19\n"
-   "00000 99999 C42      2019-090B   44865 Beidou-3 M20\n"
-   "00000 99999 C43      2019-078B   44794 Beidou-3 M22\n"
-   "00000 99999 C44      2019-078A   44793 Beidou-3 M21\n"
-   "00000 99999 C45      2019-061B   44543 Beidou-3 M24\n"
-   "00000 99999 C46      2019-061A   44542 Beidou-3 M23\n"
-   "00000 99999 C59      2018-085A   43683 Beidou-3 G1\n"
-   "00000 99999 C60      2020-017A   45344 Beidou-3 G2\n"
-   "00000 99999 J01      2010-045A   37158 QZS-1 (MICHIBIKI)\n"
-   "00000 99999 J02      2017-028A   42738 QZS-2\n"
-   "00000 99999 J03      2017-062A   42965 QZS-4\n"
-   "00000 99999 J04      2021-096A   49336 QZS-1R\n"
-   "00000 99999 J07      2017-048A   42917 QZS-3\n";
-
 int main( void)
 {
-   FILE *ifile = fopen( "I14.ATX", "rb");
+   FILE *ifile = fopen( "I20.ATX", "rb");
    char name[BUFF_SIZE], from[BUFF_SIZE], until[BUFF_SIZE], buff[BUFF_SIZE];
    bool done = false;
    time_t t0 = time( NULL);
 
    if( !ifile)
       {
-      printf( "This program needs the file I14.ATX.  Download it from\n"
-              "ftp://ftp.aiub.unibe.ch/BSWUSER52/GEN/I14.ATX\n");
+      printf( "This program needs the file I20.ATX.  Download it from\n"
+              "ftp://ftp.aiub.unibe.ch/BSWUSER54/REF/I20.ATX\n");
       return( -1);
       }
    *name = '\0';
@@ -226,6 +177,5 @@ int main( void)
          strcpy( until, buff);
       }
    fclose( ifile);
-   printf( "%s", beidou_and_qzs);
    return( 0);
 }
